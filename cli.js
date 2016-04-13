b64322f8ac8a8015c39246721128ff04bfe729e1
Cole R Lawrence <msgzht@gmail.com>
2016-04-13 12:49:29 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -6,16 +6,28 @@ module.exports = function(exec_name) {
 	var debug = require('debug')('mongodb-prebuilt:' + exec_name);
 	var exec_path = require('./index').bin_path();
 	var exec_bin = exec_path + exec_name;
+	var exec_args = process.argv.slice(2)
 
 	debug("exec_path: %s", exec_bin);
-	var child = proc.spawn(exec_bin, process.argv.slice(2), {stdio: 'inherit'});
-		child.on('close', function (code) {
-  		process.exit(code);
+	debug("exec_args: %s", exec_args);
+	
+  var oldSpawn = proc.spawn;
+  proc.spawn = function debugSpawn() {
+    debug('spawn called:')
+    debug(arguments)
+    var result = oldSpawn.apply(this, arguments);
+    return result;
+  };
+
+	var child = proc.spawn(exec_bin, exec_args, {stdio: 'inherit'});
+	child.on('close', function (code) {
+  	process.exit(code);
 	});
 
 	var monitor_child = proc.spawn(
-		exec_path + "mongokiller", 
-		[process.pid, child.pid], 
+		"node",
+		[ path.join(__dirname, "binjs", "mongokiller.js"),
+		process.pid, child.pid], 
 		{stdio: 'inherit', detached: false}
 	);
 }
